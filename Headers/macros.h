150a955cdf5f14f4bd1379ea3e33faf872bbbc96
kand1bober <kurnevich.slava@gmail.com>
2024-09-28 19:34:16 +0300

    same

--- Headers/macros.h
+++ Headers/macros.h
@@ -14,30 +14,30 @@
     // #define ON_DEBUG_FUNCS(file, func, line) , file, func, line
 
     /******** macro's of definition and prototype********/
-    #define STACK_CTOR(expr1, expr2) (StackCtor(expr1, expr2, const char* file, const char* func, const int line))
-    #define STACK_DTOR(expr) (StackDtor(expr, const char* file, const char* func, const int line))
-    #define STACK_PUSH(expr1, expr2) (StackPush(expr1, expr2, const char* file, const char* func, const int line))
-    #define STACK_POP(expr)  (StackPop(expr, const char* file, const char* func, const int line))
+    #define STACK_CTOR(expr1, expr2)  StackCtor(expr1, expr2, const char* file, const char* func, const int line)
+    #define STACK_DTOR(expr)          StackDtor(expr, const char* file, const char* func, const int line)
+    #define STACK_PUSH(expr1, expr2)  StackPush(expr1, expr2, const char* file, const char* func, const int line)
+    #define STACK_POP(expr)           StackPop(expr, const char* file, const char* func, const int line)
 
     /******** macro's of calling's of function's********/
-    #define STACK_CTOR_CALL(expr1, expr2) StackCtor(expr1, expr2, __FILE__, __PRETTY_FUNCTION__, __LINE__)
-    #define STACK_DTOR_CALL(expr) StackDtor(expr, __FILE__, __PRETTY_FUNCTION__, __LINE__)
-    #define STACK_PUSH_CALL(expr1, expr2) StackPush(expr1, expr2, __FILE__, __PRETTY_FUNCTION__, __LINE__)
-    #define STACK_POP_CALL(expr) StackPop(expr, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+    #define STACK_CTOR_CALL(expr1, expr2)  StackCtor(expr1, expr2, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+    #define STACK_DTOR_CALL(expr)          StackDtor(expr, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+    #define STACK_PUSH_CALL(expr1, expr2)  StackPush(expr1, expr2, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+    #define STACK_POP_CALL(expr)           StackPop(expr, __FILE__, __PRETTY_FUNCTION__, __LINE__)
 
 #else
     // #define ON_DEBUG_FUNCS(file, func, line)
 
     /******** macro's of definition and prototype********/
-    #define STACK_CTOR(expr1, expr2) (StackCtor(expr1, expr2))
-    #define STACK_DTOR(expr) (StackDtor(expr))
-    #define STACK_PUSH(expr1, expr2) (StackPush(expr1, expr2))
-    #define STACK_POP(expr)  (StackPop(expr))
+    #define STACK_CTOR(expr1, expr2)  StackCtor(expr1, expr2)
+    #define STACK_DTOR(expr)          StackDtor(expr)
+    #define STACK_PUSH(expr1, expr2)  StackPush(expr1, expr2)
+    #define STACK_POP(expr)           StackPop(expr)
 
     /******** macro's of calling's of function's********/
-    #define STACK_CTOR_CALL(expr1, expr2) StackCtor(expr1, expr2)
-    #define STACK_DTOR_CALL(expr) StackDtor(expr)
-    #define STACK_PUSH_CALL(expr1, expr2) StackPush(expr1, expr2)
-    #define STACK_POP_CALL(expr) StackPop(expr)
+    #define STACK_CTOR_CALL(expr1, expr2)  StackCtor(expr1, expr2)
+    #define STACK_DTOR_CALL(expr)          StackDtor(expr)
+    #define STACK_PUSH_CALL(expr1, expr2)  StackPush(expr1, expr2)
+    #define STACK_POP_CALL(expr)           StackPop(expr)
 
 #endif
